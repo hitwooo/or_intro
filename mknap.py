d7f3fc5fb7c34c4d3a227e955d4c2ddc33acd453
hitwooo <nguyenhaithuu@gmail.com>
2024-03-12 19:32:32 +0700

    manipulate data

--- mknap.py
+++ mknap.py
@@ -1,5 +1,35 @@
 # %%
 from docplex.mp.model import Model
+import re
+# %% Functions for data extracting
+def extract_data(block):
+    one_line_data = ' '.join(block.split())
+    parts = one_line_data.split()
+    n, m, optimal_value = int(parts[0]), int(parts[1]), float(parts[2])
+    p = [float(x) for x in parts[3:3+n]]
+    constraint_data = parts[3+n:-m]
+    r = [constraint_data[i*n:(i+1)*n] for i in range(m)]
+    b = [float(x) for x in parts[-m:]]
+    return n, m, optimal_value, p, r, b
+
+def read_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    blocks = re.split(r'\n\s*\n+', content.strip())
+    data_list = []  
+    for index, block in enumerate(blocks[1:]):
+        n, m, optimal_value, p, r, b = extract_data(block)  
+        data_list.append({
+            'problem_number': index + 1, 
+            'n': n,
+            'm': m,
+            'optimal_value': optimal_value,
+            'p': p,
+            'r': r,
+            'b': b        
+        })
+    return data_list
+
 # %% Function to solve multidimensional knapsack problem
 def solve_mknap(n,m,p,r,b):
     #define model
@@ -25,19 +55,15 @@ def solve_mknap(n,m,p,r,b):
                 print(f"Item {j} is selected.")
     else:
         print("No solution found.")
-# %% test
-n = 6
-m = 10
-p = [100, 600, 1200, 2400, 500, 2000]
-r = [[8, 12, 13, 64, 22, 41],
-     [8, 12, 13, 75, 22, 41],
-     [3, 6, 4, 18, 6, 4],
-     [5, 10, 8, 32, 6, 12],
-     [5, 13, 8, 42, 6, 20],
-     [5, 13, 8, 48, 6, 20],
-     [0, 0, 0, 0, 8, 0],
-     [3, 0, 4, 0, 8, 0],
-     [3, 2, 4, 0, 8, 4],
-     [3, 2, 4, 8, 8, 4]]
-b = [80, 96, 20, 36, 44, 48, 10, 18, 22, 24]
-solve_mknap(n, m, p, r, b)
+
+#% file
+file_path = '/Users/hitwooo/Downloads/mknap1.txt'
+data_list = read_file(file_path)
+#for data in data_list:
+#    print(f"Solving multiple knapsack problem for: {data['problem_number']}")
+#    solve_mknap(data['n'], data['m'], data['p'], data['r'], data['b'])
+
+#print(data_list[0])
+solve_mknap(data_list[0]['n'], data_list[0]['m'], data_list[0]['p'], data_list[0]['r'], data_list[0]['b'])
+
+ 
